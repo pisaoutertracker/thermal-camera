ea45a56d4a7a75f35b6bcfe5267dac02d4196740
Filippo Cattafesta <filippo.cattafesta99@gmail.com>
2023-11-22 15:36:27 +0100

    Test for Flask Restful

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
 from thermalcamera import ThermalCamera
 
 app = Flask(__name__)
+api = Api(app)
 
-thermal_camera = ThermalCamera(0)
-
-
-@app.route("/frame")
-def some_wrap(func):
-    def wrapper(*args, **kwargs):
-        return jsonify(func(*args, **kwargs))
-
-    return wrapper
-
-
+class Initiate(Resource):
+    def post(self):
+        args = request.get_json()
+        thermal_camera = ThermalCamera(**args)
+        thermal_camera.initiate()
+        return jsonify({'status': 'success'}), 200
+    
+api.add_resource(Initiate, '/initiate')
+    
 if __name__ == "__main__":
     app.run(debug=True)
