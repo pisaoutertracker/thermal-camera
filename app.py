69defcb39f7d2b1226e914a355a1fe757aef2dd3
Filippo Cattafesta <filippo.cattafesta99@gmail.com>
2023-11-22 16:05:47 +0100

    Test for Flask Restful

--- app.py
+++ app.py
@@ -6,14 +6,21 @@ from thermalcamera import ThermalCamera
 app = Flask(__name__)
 api = Api(app)
 
+thermal_camera = ThermalCamera()
 class Initiate(Resource):
     def post(self):
         args = request.get_json()
-        thermal_camera = ThermalCamera(**args)
-        thermal_camera.initiate()
-        return jsonify({'status': 'success'}), 200
+        thermal_camera.absolute_position = args['absolute_position']
+        return {'status': 'success'}, 200
+    
+class GetFrame(Resource):
+    def get(self):
+        args = request.get_json()
+        thermal_camera.rotate(args['angle'])
+        return thermal_camera.get_frame_as_bytes(), 200
     
 api.add_resource(Initiate, '/initiate')
+api.add_resource(GetFrame, '/get_frame')
     
 if __name__ == "__main__":
     app.run(debug=True)
