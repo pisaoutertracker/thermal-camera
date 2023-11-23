2c2587cc43a9355ff234bebb96b369ad51089b72
Filippo Cattafesta <filippo.cattafesta99@gmail.com>
2023-11-23 10:11:08 +0100

    Other operations added

--- thermalcamera.py
+++ thermalcamera.py
@@ -1,3 +1,4 @@
+"""Thermal camera system basic operations."""
 import time
 import struct
 import board
@@ -9,11 +10,13 @@ import adafruit_mlx90640
 from adafruit_motor import stepper
 from adafruit_motorkit import MotorKit
 
+import RPi.GPIO as GPIO
+
 
 class ThermalCamera:
     """Class for the Thermal Camera system."""
 
-    def __init__(self, absolute_position):
+    def __init__(self, absolute_position=None):
         # Thermal camera setup
         self.mlx = adafruit_mlx90640.MLX90640(
             busio.I2C(board.SCL, board.SDA, frequency=int(1e6))
@@ -21,9 +24,24 @@ class ThermalCamera:
         self.mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
         # Stepper motor setup
         self.kit = MotorKit(i2c=board.I2C())
-        self._absolute_position = (
-            absolute_position  # Absolute position of the stepper motor in degrees
-        )
+        # Absolute position of the stepper motor in degrees
+        if absolute_position is None:
+            print("No absolute position given, using last position.")
+            self._absolute_position = self.import_absolute_position()
+        else:
+            self._absolute_position = absolute_position
+
+    @property
+    def absolute_position(self):
+        """Get the absolute position of the stepper motor."""
+        return self._absolute_position
+
+    @absolute_position.setter
+    def absolute_position(self, value):
+        """Set the absolute position of the stepper motor."""
+        if value < 0 or value > 360:
+            raise ValueError("Absolute position must be between 0 and 360 degrees.")
+        self._absolute_position = value
 
     def get_frame(self):
         """Get a frame from the thermal camera."""
@@ -47,20 +65,43 @@ class ThermalCamera:
         for i in range(int(angle / 1.8)):
             self.kit.stepper1.onestep(*args, **kwargs)
             time.sleep(0.01)
-        self._absolute_position += angle
+        self.absolute_position = (self.absolute_position + angle) % 360
         print("Done.")
 
-    @property
-    def absolute_position(self):
-        """Get the absolute position of the stepper motor."""
-        return self._absolute_position
+    def go_to(self, position, *args, **kwargs):
+        """Go to a given position."""
+        print(f"Going to position {position}...")
+        self.rotate(position - self.absolute_position, *args, **kwargs)
+        print("Done.")
 
-    @absolute_position.setter
-    def absolute_position(self, value):
-        """Set the absolute position of the stepper motor."""
-        self._absolute_position = value
-
-    def _export_absolute_position(self):
+    def export_absolute_position(self):
         """Export the absolute position of the stepper motor."""
-        with open("absolute_position.txt", "w") as f:
-            f.write(str(self.absolute_position))
+        with open("absolute_position.csv", "a") as f:
+            f.write(f"{time.strftime('%Y-%m-%d %H:%M:%S')},{self.absolute_position}\n")
+        print("Exported absolute position.")
+
+    def import_absolute_position(self):
+        """Import the absolute position of the stepper motor."""
+        with open("absolute_position.csv", "r") as f:
+            lines = f.readlines()
+        self.absolute_position = float(lines[-1].split(",")[1])
+        print("Imported absolute position.")
+
+    def _calibrate(self):  # ! Still under development
+        """Calibrate the stepper motor."""
+        print("Calibrating stepper motor...")
+        # Set up the GPIO pins
+        GPIO.setmode(GPIO.BCM)
+        pin = 18  # ! Update with the correct pin
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        # Calibrate the stepper motor
+        while True:
+            state = GPIO.input(pin)
+            if state == GPIO.HIGH:
+                self.absolute_position = 0
+                print("Sensor found: absolute position set to 0 degrees.")
+                GPIO.cleanup()
+                break
+            else:
+                self.kit.stepper1.onestep()
+                time.sleep(0.01)
